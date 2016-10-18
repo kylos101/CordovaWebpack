dd778c5a6959e3ac93b818aaa22a0702b010d2fd
Brennan <KB79729@AD1.PROD>
2016-10-17 23:38:05 -0400

    Add more comments

--- scripts/go.bat
+++ scripts/go.bat
@@ -1,16 +1,12 @@
 echo off
 REM Our Cordova dev loop
+
+REM make sure our www directory exists otherwise Cordova commands won't work
 if not exist www md www
+
+REM Build once and then setup a file watcher to do subsequent builds
 start cmd /k "cordova build browser&&node watch.js""
+
+REM Serve built content
 start cmd /k "cordova serve 8001""
 cls
-
-REM Runs afterPrepare hooks
-REM phonegap run browser --verbose 
-
-REM Does NOT run afterPrepare hooks
-REM phonegap serve --verbose
-
-REM For debugging over the network
-REM use current ip address
-REM weinre --boundHost 192.168.1.145
--- watch.js
+++ watch.js
@@ -1,37 +1,43 @@
 #!/usr/bin/env node
 
+// require some node modules (node and npm are out of scope unless there's time)
 var watch = require('watch'),
 fs = require('fs'),
-path = require('path'),        
+path = require('path'),
 exec = require('child_process').exec;
 
+// state of whether this has been triggered
 var triggered = false;
 
+// utility function to output errors
 function puts(error, stdout, stderr) {
     console.log(stdout)
 
     if (stderr || error)
-    {        
+    {
         console.log(".");
         console.log(stderr);
         console.log(".");
         console.log(error);
         console.log(".");
         console.log("Change a file and try again...");
-        console.log(".");        
+        console.log(".");
     }
 }
 
+// get the path to the src folder
 var thePath = path.normalize(path.join(process.cwd(), "src"));
+
+// establish a file watcher for the src folder
 watch.createMonitor(thePath, function (monitor) {
-        
+
     monitor.on("created", function (f, stat) {
         if (!triggered)
         {
             triggered = true;
             exec("cordova build browser&&echo built on: %DATE% %TIME%", puts);
             triggered = false;
-        }      
+        }
     });
     monitor.on("changed", function (f, curr, prev) {
         if (!triggered)
@@ -39,7 +45,7 @@ watch.createMonitor(thePath, function (monitor) {
             triggered = true;
             exec("cordova build browser&&echo built on: %DATE% %TIME%", puts);
             triggered = false;
-        }       
+        }
     });
     monitor.on("removed", function (f, stat) {
         if (!triggered)
@@ -47,6 +53,6 @@ watch.createMonitor(thePath, function (monitor) {
             triggered = true;
             exec("cordova build browser&&echo built on: %DATE% %TIME%", puts);
             triggered = false;
-        }       
-    });  
+        }
+    });
 });
